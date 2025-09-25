e0757e1969d21b92d54cb7fd3c470a545815514f
Akakkpo <hova1607@gmail.com>
2025-09-25 11:56:19 +0200

    le premier enregistrement

--- GestionEmail/MainWindow.xaml.cs
+++ GestionEmail/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Net.Mail;
+using System.Net;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,41 @@ namespace GestionEmail
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+      
+        private void TxtText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+               
+                string depart  = TxtEmail.Text.Trim();
+                string destination= TxtDestiation.Text.Trim();
+                string mdp = TxtMdp.Text.Trim();
+
+                string sujet = TxtBonjour.Text.Trim();
+
+                string corps= TxtText.Text.Trim();
+
+                MailMessage mail = new MailMessage(depart, destination, sujet, corps);
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.Credentials = new NetworkCredential(depart, mdp);
+                smtp.EnableSsl = true;
+
+                // Envoi
+                smtp.Send(mail);
+
+                MessageBox.Show("Email envoyé ✅");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex.Message);
+            }
         }
     }
 }
